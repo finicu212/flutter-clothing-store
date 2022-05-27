61ce50c2720c4ad1ceee385f58084014b8040dcd
minculeteandrei <minculeteandrei@gmail.com>
2022-05-27 18:56:10 +0300

    final hw

--- lib/main.dart
+++ lib/main.dart
@@ -97,6 +97,15 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
               child: const Text("Submit")
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const MyHomePage2(title: "home page 2")),
+                    );
+                  },
+                  child: const Text("Go to main page 2")
               )
             ],
           ),
@@ -106,3 +115,110 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+
+class MyApp2 extends StatelessWidget {
+  const MyApp2({Key? key}) : super(key: key);
+
+  static const String loginTitle = "Login Page";
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: loginTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage2(title: loginTitle),
+    );
+  }
+}
+
+class MyHomePage2 extends StatefulWidget {
+  const MyHomePage2({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage2> createState() => _MyHomePageState2();
+}
+
+class _MyHomePageState2 extends State<MyHomePage2> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: Container(
+            width: 300.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                      children: const <Widget>[
+                        Text("Username:"),
+                        Expanded(
+                            child: SizedBox(
+                                height: 50.0,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    label: Text("Username"),
+                                    border: OutlineInputBorder(),
+                                    hintText: 'username',
+                                  ),
+                                )
+                            )
+                        )
+                      ]
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                      children: const <Widget>[
+                        Text("Password:"),
+                        Expanded(
+                            child:SizedBox(
+                                height: 50.0,
+                                child: TextField(
+                                  obscureText: true,
+                                  enableSuggestions: false,
+                                  autocorrect: false,
+                                  decoration: InputDecoration(
+                                    label: Text("Password"),
+                                    border: OutlineInputBorder(),
+                                    hintText: 'password',
+                                  ),
+                                )
+                            )
+                        )
+                      ]
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MyShopPage(title: "Shop page")),
+                      );
+                    },
+                    child: const Text("Submit")
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Go back")
+                )
+              ],
+            ),
+          )
+      ),
+    );
+  }
+}
